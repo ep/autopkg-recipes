95759dde705f8b5e7e7bcc22e12705102cc03d0b
derek <derek.marcotte@experiencepoint.com>
2015-05-11 13:21:48 -0400

    added version number to downloader

--- Glance/GlanceURLProvider.py
+++ Glance/GlanceURLProvider.py
@@ -27,7 +27,8 @@ __all__ = ["GlanceURLProvider"]
 GLANCE_DOWNLOAD_URL = "http://www.glance.net/install/install.asp"
 RE_GLANCE_ZIP = re.compile(
 	r'href="(?P<url>/install/GlanceMac_[^"]+\.zip)"', re.I)
-
+RE_GLANCE_VERSION = re.compile(
+	r'Version (?P<version>[0-9]+(.[0-9]+)*)"', re.I)
 
 class GlanceURLProvider(Processor):
 	"""Provides URL to the latest binary release of Glance."""
@@ -42,10 +43,17 @@ class GlanceURLProvider(Processor):
 		"url": {
 			"description": "URL to the latest binary release of Glance.",
 		},
+		"version": {
+			"description": "Version of the latest release of Glance.",
+		},
 	}
 
-	def get_glance_zip_url(self, download_url):
-		"""Find download URL for zip download"""
+
+	def get_glance_page(self, download_url):
+		"""Download and cache copy of Glance download page"""
+		if (self.page is not None):
+			return self.page
+
 		#pylint: disable=no-self-use
 		# Read HTML index.
 		try:
@@ -58,6 +66,13 @@ class GlanceURLProvider(Processor):
 		except BaseException as err:
 			raise ProcessorError("Can't download %s: %s" % (download_url, err))
 
+		self.page = html
+		return self.page
+
+	def get_glance_zip_url(self, download_url):
+		"""Find download URL for zip download"""
+		html = self.get_glance_page(download_url)
+
 		# Search for download link.
 		match = RE_GLANCE_ZIP.search(html)
 		if not match:
@@ -67,12 +82,28 @@ class GlanceURLProvider(Processor):
 		# Return URL.
 		return match.group("url")
 
+	def get_glance_version(self, download_url):
+		"""Find version for mac download"""
+		html = self.get_glance_page(download_url)
+
+		# Search for download link.
+		match = RE_GLANCE_VERSION.search(html)
+		if not match:
+			raise ProcessorError(
+				"Couldn't find Glance version in %s" % download_url)
+
+		# Return URL.
+		return match.group("version")
+
 	def main(self):
 		# Determine base_url.
 		download_url = self.env.get('download_url', GLANCE_DOWNLOAD_URL)
 
 		self.env["url"] = "http://www.glance.net" + self.get_glance_zip_url(download_url)
 		self.output("Found URL %s" % self.env["url"])
+
+		self.env["version"] = self.get_glance_version(download_url)
+		self.output("Found version %s" % self.env["version"])
 
 
 if __name__ == '__main__':
